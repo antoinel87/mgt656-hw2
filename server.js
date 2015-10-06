57d12a9485fcff77b571cc1560f80bd2acdb7e9c
antoinel87 <antoine.ligier@yale.edu>
2015-10-06 03:29:31 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I totally am a frickin ninja - hellz yes!');
+});
+
+app.get('/quaint-lion', function(request, response){
+    response.send('apl34');
 });
 
 app.listen(process.env.PORT || 4000);
